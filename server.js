65f540ea3c62c062c413241f46911a2837a02219
ginajonas <ginajonas7@gmail.com>
2021-01-27 18:11:06 -0500

    Added sessions authentication

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express')
-
+var session = require('express-session')
+var cookieParser = require('cookie-parser')
 const mongoose = require('mongoose')
+
 const app = express()
 const PORT = process.env.PORT || 3001
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(cookieParser())
+app.use(session({ secret: 'Your secret key' }))
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))

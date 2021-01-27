d70d61d8e29ff0ae21b6aa5596b021ed7fe3fc0a
ginajonas <ginajonas7@gmail.com>
2021-01-27 18:15:47 -0500

    added express session

--- client/src/components/SignUpForm.jsx
+++ client/src/components/SignUpForm.jsx
@@ -1,52 +1,97 @@
+import axios from 'axios'
+import { useState } from 'react'
+
 export default function SignUpForm() {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [repeatPassword, setRepeatPassword] = useState('')
+
+  const signupSubmit = (e) => {
+    e.preventDefault()
+    if (repeatPassword !== password) {
+      alert("Passwords don't match")
+      return
+    }
+    axios
+      .post('/api/signup', {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
+      })
+      .then((response) => {
+        window.location = '/'
+      })
+      .catch((error) => {
+        alert(error.response.data.message)
+      })
+  }
+
   return (
-    <form>
+    <form onSubmit={signupSubmit}>
       <div className="container">
         <h1>Sign Up</h1>
         <p>Please fill in this form to create an account.</p>
         <hr />
-        <label for="first name">
+        <label htmlFor="first name">
           <b>First name</b>
         </label>
         <input
           type="text"
           placeholder="Enter First Name"
           name="first name"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
           required
         />
 
-        <label for="last name">
+        <label htmlFor="last name">
           <b>Last Name</b>
         </label>
         <input
           type="text"
           placeholder="Enter Last Name"
           name="last name"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
           required
         />
 
-        <label for="email">
+        <label htmlFor="email">
           <b>Email</b>
         </label>
-        <input type="text" placeholder="Enter Email" name="email" required />
+        <input
+          type="text"
+          placeholder="Enter Email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
 
-        <label for="psw">
+        <label htmlFor="psw">
           <b>Password</b>
         </label>
         <input
           type="password"
           placeholder="Enter Password"
           name="psw"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           required
         />
 
-        <label for="psw-repeat">
+        <label htmlFor="psw-repeat">
           <b>Repeat Password</b>
         </label>
         <input
           type="password"
           placeholder="Repeat Password"
           name="psw-repeat"
+          value={repeatPassword}
+          onChange={(e) => setRepeatPassword(e.target.value)}
           required
         />
         <button type="submit">Sign Up</button>

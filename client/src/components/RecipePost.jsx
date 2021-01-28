6a7789c60d54059c64459846bb237e3ce6a4a9c7
ginajonas <ginajonas7@gmail.com>
2021-01-27 20:27:14 -0500

    Added event listerner

--- client/src/components/RecipePost.jsx
+++ client/src/components/RecipePost.jsx
@@ -1,4 +1,10 @@
-export default function RecipePost({ recipe }) {
+import axios from 'axios'
+
+export default function RecipePost({ recipe, user, _id }) {
+  const likeRecipe = (e) => {
+    e.preventDefault()
+    axios.post('/api/likeRecipe', { recipe: _id }).then((response) => {})
+  }
   return (
     <div className="w3-container w3-card w3-white w3-round w3-margin post">
       <br />
@@ -8,12 +14,18 @@ export default function RecipePost({ recipe }) {
         className="w3-left w3-circle w3-margin-right"
       />
       <span className="w3-right w3-opacity">16 min</span>
-      <h4>Jane Doe</h4>
+      <h4>
+        Chef {user.firstName} {user.lastName}
+      </h4>
       <br />
       <hr className="w3-clear" />
       <p>{recipe}</p>
-      <button type="button" className="w3-button w3-theme-d1 w3-margin-bottom">
-        <i className="fa fa-thumbs-up"></i> &nbsp;Like
+      <button
+        onClick={likeRecipe}
+        type="button"
+        className="w3-button w3-theme-d1 w3-margin-bottom"
+      >
+        <i className="fa fa-book"></i> &nbsp;Like Recipe
       </button>
       <button type="button" className="w3-button w3-theme-d2 w3-margin-bottom">
         <i className="fa fa-comment"></i> &nbsp;Comment

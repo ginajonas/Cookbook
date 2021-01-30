d4eff6ed46dee8ee0aa83b3a5d8251fa74e3640b
ginajonas <ginajonas7@gmail.com>
2021-01-30 15:27:23 -0500

    Changed collection name from recipes to recipe

--- models/recipe.js
+++ models/recipe.js
@@ -26,6 +26,6 @@ const recipeSchema = new Schema(
   { timestamps: true }
 )
 
-const Recipes = mongoose.model('Recipes', recipeSchema)
+const Recipe = mongoose.model('Recipe', recipeSchema)
 
-module.exports = Recipes
+module.exports = Recipe

ee4bbd085be15ac3b730650babf669f7c02156ed
ginajonas <ginajonas7@gmail.com>
2021-01-29 19:05:42 -0500

    Removed on click

--- client/src/components/Accordion.jsx
+++ client/src/components/Accordion.jsx
@@ -13,20 +13,14 @@ export default function Accordion() {
         <div id="Demo1" className="w3-hide w3-container">
           <p>Some text..</p>
         </div>
-        <button
-          onClick="myFunction('Demo2')"
-          className="w3-button w3-block w3-theme-l1 w3-left-align"
-        >
+        <button className="w3-button w3-block w3-theme-l1 w3-left-align">
           <i className="fa fa-calendar-check-o fa-fw w3-margin-right"></i> My
           Events
         </button>
         <div id="Demo2" className="w3-hide w3-container">
           <p>Some other text..</p>
         </div>
-        <button
-          onClick="myFunction('Demo3')"
-          className="w3-button w3-block w3-theme-l1 w3-left-align"
-        >
+        <button className="w3-button w3-block w3-theme-l1 w3-left-align">
           <i className="fa fa-users fa-fw w3-margin-right"></i> My Photos
         </button>
         <div id="Demo3" className="w3-hide w3-container">
